6f5af0811d019add64d4cf1387f663a5d5c5aa94
Michael Aaron Murphy <mmstick@pm.me>
2020-02-26 15:41:20 -0700

    chore: Remove logs from grab_op module

--- src/grab_op.ts
+++ src/grab_op.ts
@@ -1,7 +1,6 @@
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 import * as Lib from 'lib';
-import * as Log from 'log';
 
 import type { Entity } from 'ecs';
 import type { Rectangle } from 'rectangle';
@@ -16,43 +15,33 @@ export class GrabOp {
     }
 
     operation(change: Rectangle): Lib.Movement {
-        Log.debug(`changing from (${this.rect.fmt()}) to (${change.fmt()})`);
         const xpos = this.rect.x == change.x;
         const ypos = this.rect.y == change.y;
 
         if (xpos && ypos) {
             if (this.rect.width == change.width) {
                 if (this.rect.height == change.width) {
-                    Log.debug(`no movement detected`);
                     return Lib.Movement.NONE;
                 } else if (this.rect.height < change.height) {
-                    Log.debug(`grow down`);
                     return Lib.Movement.GROW | Lib.Movement.DOWN;
                 } else {
-                    Log.debug(`shrink up`);
                     return Lib.Movement.SHRINK | Lib.Movement.UP;
                 }
             } else if (this.rect.width < change.width) {
-                Log.debug(`grow right`);
                 return Lib.Movement.GROW | Lib.Movement.RIGHT;
             } else {
-                Log.debug(`shrink left`);
                 return Lib.Movement.SHRINK | Lib.Movement.LEFT;
             }
         } else if (xpos) {
             if (this.rect.height < change.height) {
-                Log.debug(`grow up`);
                 return Lib.Movement.GROW | Lib.Movement.UP;
             } else {
-                Log.debug(`shrink down`);
                 return Lib.Movement.SHRINK | Lib.Movement.DOWN;
             }
         } else if (ypos) {
             if (this.rect.width < change.width) {
-                Log.debug(`grow left`);
                 return Lib.Movement.GROW | Lib.Movement.LEFT;
             } else {
-                Log.debug(`shrink right`);
                 return Lib.Movement.SHRINK | Lib.Movement.RIGHT;
             }
         } else {
