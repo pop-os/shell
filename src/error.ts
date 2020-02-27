02fdf0331b6139ed6a3c08465600615ee0f9df0e
Michael Aaron Murphy <mmstick@pm.me>
2020-02-26 17:18:32 -0700

    feat: Add error type to extension

---
+++ src/error.ts
@@ -0,0 +1,36 @@
+export class Error {
+    reason: string;
+
+    cause: Error | null = null;
+
+    constructor(reason: string) {
+        this.reason = reason;
+    }
+
+    context(why: string): Error {
+        let error = new Error(why);
+        error.cause = this;
+        return error;
+    }
+
+    * chain(): IterableIterator<Error> {
+        let current: Error | null = this;
+
+        while (current != null) {
+            yield current;
+            current = current.cause;
+        }
+    }
+
+    format(): string {
+        let causes = this.chain();
+
+        let buffer: string = causes.next().value.reason;
+
+        for (const error of causes) {
+            buffer += `\n    caused by: ` + error.reason;
+        }
+
+        return buffer + `\n`;
+    }
+}
