77e5a91638d17754834b4cc43e4373689a71de15
Michael Aaron Murphy <mmstick@pm.me>
2020-02-27 13:19:18 -0700

    improv: Add once_cell module with OnceCell type
    Used for once-only lazy evaluation

---
+++ src/once_cell.ts
@@ -0,0 +1,13 @@
+export class OnceCell<T> {
+    value: T | undefined;
+
+    constructor() { }
+
+    get_or_init(callback: () => T): T {
+        if (this.value === undefined) {
+            this.value = callback();
+        }
+
+        return this.value
+    }
+}
