961a3b25e8e7edba0f4ae720f3dc9fb987aaee81
Mike Wallio <rawrizaur@gmail.com>
2020-05-06 17:12:49 -0400

    feat: Add live preview for extensions
    - Have calc evaluate as user types with display

--- src/launcherext.ts
+++ src/launcherext.ts
@@ -3,13 +3,15 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 const { evaluate } = Me.imports.math.math;
 const { spawnCommandLine } = imports.misc.util;
 
-const { GLib } = imports.gi;
+const { GLib, St } = imports.gi;
 
 import * as log from 'log';
 import * as once_cell from 'once_cell';
 
 import type { Ext } from 'extension';
 import type { Search } from 'search';
+
+const DEFAULT_ICON_SIZE = 34;
 
 const TERMINAL = new once_cell.OnceCell<string>();
 
@@ -29,7 +31,7 @@ export type LauncherExtension = {
 
     /**
      * Perform apply action
-     * @param text The currently typed text in input
+     * @param text The currently typed text in input minus the launcher extension prefix
      * @param index The currently selected index
      * @returns true to keep showing the launcher after apply, false to dismiss launcher
      */
@@ -56,7 +58,7 @@ export class CalcLauncher implements LauncherExtension {
         return this;
     }
 
-    apply = (expr: string): boolean => {
+    apply(expr: string): boolean {
         const value: string = evaluate(expr).toString();
         log.info(`${expr} = ${value}`);
         if (!this.search) {
@@ -66,6 +68,24 @@ export class CalcLauncher implements LauncherExtension {
         this.search?.set_text(`=${value}`);
 
         return true;
+    }
+
+    search_results(expr: string): Array<[string, St.Widget, St.Widget]> | null {
+        const item: [string, St.Widget, St.Widget] =
+            [
+                `=${evaluate(expr).toString()}`,
+                new St.Icon({
+                    icon_name: 'x-office-spreadsheet', // looks like calculations?
+                    icon_size: this.search?.icon_size() ?? DEFAULT_ICON_SIZE / 2,
+                    style_class: "pop-shell-search-cat"
+                }),
+                new St.Icon({
+                    icon_name: 'accessories-calculator',
+                    icon_size: this.search?.icon_size() ?? DEFAULT_ICON_SIZE
+                })
+            ];
+
+        return [item];
     }
 }
 
