4d9647d741e8bee2d1f3b4622cb558e5cd8b3564
Mike Wallio <rawrizaur@gmail.com>
2020-05-07 21:55:02 -0400

    fix: Use gicon instead of icon path
    - Use Gio for icon loading
    - Works with other app sources

--- src/launcher.ts
+++ src/launcher.ts
@@ -131,7 +131,7 @@ export class Launcher extends search.Search {
                             style_class: "pop-shell-search-cat"
                         }),
                         new St.Icon({
-                            icon_name: app.icon() ?? 'applications-other',
+                            gicon: app.icon(),
                             icon_size: ICON_SIZE
                         })
                     ];
