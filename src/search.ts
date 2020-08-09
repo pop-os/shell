f4180eedba327c29e4f8da5354d67bd2ce0a62ae
Brandon Marlowe <bpmarlowe-software@protonmail.com>
2020-08-09 18:23:33 -0400

    added emacs ctrl-n and ctrl-p bindings for search in launcher

--- src/search.ts
+++ src/search.ts
@@ -88,7 +88,7 @@ export class Search {
             }
 
             let s = event.get_state();
-            if (c == 65362 || (s == Clutter.ModifierType.CONTROL_MASK && c == 107)) {
+            if (c == 65362 || (s == Clutter.ModifierType.CONTROL_MASK && c == 107) || (s == Clutter.ModifierType.CONTROL_MASK && c == 112)) {
                 // Up arrow was pressed
                 if (0 < this.active_id) {
                     this.unselect();
@@ -100,7 +100,7 @@ export class Search {
                 	this.active_id = this.widgets.length - 1;
                 	this.select();
                 }
-            } else if (c == 65364 || (s == Clutter.ModifierType.CONTROL_MASK && c == 106)) {
+            } else if (c == 65364 || (s == Clutter.ModifierType.CONTROL_MASK && c == 106) || (s == Clutter.ModifierType.CONTROL_MASK && c == 110)) {
                 // Down arrow was pressed
                 if (this.active_id + 1 < this.widgets.length) {
                     this.unselect();
