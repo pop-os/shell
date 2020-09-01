a7e681caffd7d91a3884f08f0957b7a0772f8d84
Michael Aaron Murphy <mmstick@pm.me>
2020-09-01 09:10:16 -0600

    feat(search): Clickable search results
    Closes #260

--- src/widgets.ts
+++ src/widgets.ts
@@ -3,7 +3,7 @@ const { Clutter, Pango, St } = imports.gi;
 export class Box {
     container: any;
 
-    constructor(args: Object | null) {
+    constructor(args: Object) {
         this.container = new St.BoxLayout(args);
     }
 
@@ -13,21 +13,20 @@ export class Box {
     }
 }
 
-export class ApplicationBox extends Box {
-    constructor(title: string, category_icon: St.Widget, app_icon: St.Widget) {
-        super({ styleClass: "pop-shell-search-element" });
-        category_icon.set_y_align(Clutter.ActorAlign.CENTER);
-        app_icon.set_y_align(Clutter.ActorAlign.CENTER);
+export function application_button(title: string, category_icon: St.Widget, app_icon: St.Widget): St.Widget {
+    let layout = new Box({});
+    category_icon.set_y_align(Clutter.ActorAlign.CENTER);
+    app_icon.set_y_align(Clutter.ActorAlign.CENTER);
 
-        let label = new St.Label({
-            text: title,
-            styleClass: "pop-shell-search-label",
-            y_align: Clutter.ActorAlign.CENTER
-        });
+    let label = new St.Label({
+        text: title,
+        styleClass: "pop-shell-search-label",
+        y_align: Clutter.ActorAlign.CENTER
+    });
 
-        label.clutter_text.set_ellipsize(Pango.EllipsizeMode.END);
-        this.add(category_icon)
-            .add(app_icon)
-            .add(label);
-    }
+    label.clutter_text.set_ellipsize(Pango.EllipsizeMode.END);
+    layout.add(category_icon).add(app_icon).add(label);
+    let container = new St.Button({ styleClass: "pop-shell-search-element" });
+    container.add_actor(layout.container);
+    return container;
 }
