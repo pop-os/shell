0fce7eb6d95d6ce2142cc8fe4ba2b55d6bb7ec3f
Michael Aaron Murphy <mmstick@pm.me>
2020-10-05 14:17:55 -0600

    fix: Support static workspaces with workspace management in Pop Shell

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const extension = ExtensionUtils.getCurrentExtension();
 const { Gio, Gdk } = imports.gi;
 
-const DARK = ['dark', 'adapta', 'plata', 'dracula']
+const DARK = ["dark", "adapta", "plata", "dracula"]
 
 interface Settings extends GObject.Object {
     get_boolean(key: string): boolean;
@@ -21,7 +21,7 @@ function settings_new_id(schema_id: string): Settings | null {
     try {
         return new Gio.Settings({ schema_id });
     } catch (err) {
-        if (schema_id !== 'org.gnome.shell.extensions.user-theme') {
+        if (schema_id !== "org.gnome.shell.extensions.user-theme") {
             global.log(err)
         }
 
@@ -47,24 +47,24 @@ function settings_new_schema(schema: string): Settings {
     return new Gio.Settings({ settings_schema: schemaObj });
 }
 
-const ACTIVE_HINT = 'active-hint';
-const COLUMN_SIZE = 'column-size';
-const GAP_INNER = 'gap-inner';
-const GAP_OUTER = 'gap-outer';
-const ROW_SIZE = 'row-size';
-const SHOW_TITLE = 'show-title';
-const SMART_GAPS = 'smart-gaps';
-const SNAP_TO_GRID = 'snap-to-grid';
-const TILE_BY_DEFAULT = 'tile-by-default';
-const HINT_COLOR_RGBA = 'hint-color-rgba';
-const DEFAULT_RGBA_COLOR = 'rgba(251, 184, 108, 1)'; //pop-orange
-const LOG_LEVEL = 'log-level';
+const ACTIVE_HINT = "active-hint";
+const COLUMN_SIZE = "column-size";
+const GAP_INNER = "gap-inner";
+const GAP_OUTER = "gap-outer";
+const ROW_SIZE = "row-size";
+const SHOW_TITLE = "show-title";
+const SMART_GAPS = "smart-gaps";
+const SNAP_TO_GRID = "snap-to-grid";
+const TILE_BY_DEFAULT = "tile-by-default";
+const HINT_COLOR_RGBA = "hint-color-rgba";
+const DEFAULT_RGBA_COLOR = "rgba(251, 184, 108, 1)"; //pop-orange
+const LOG_LEVEL = "log-level";
 
 export class ExtensionSettings {
-    ext: Settings = settings_new_schema(extension.metadata['settings-schema']);
-    int: Settings | null = settings_new_id('org.gnome.desktop.interface');
-    mutter: Settings | null = settings_new_id('org.gnome.mutter');
-    shell: Settings | null = settings_new_id('org.gnome.shell.extensions.user-theme');
+    ext: Settings = settings_new_schema(extension.metadata["settings-schema"]);
+    int: Settings | null = settings_new_id("org.gnome.desktop.interface");
+    mutter: Settings | null = settings_new_id("org.gnome.mutter");
+    shell: Settings | null = settings_new_id("org.gnome.shell.extensions.user-theme");
 
     // Getters
 
@@ -74,6 +74,10 @@ export class ExtensionSettings {
 
     column_size(): number {
         return this.ext.get_uint(COLUMN_SIZE);
+    }
+
+    dynamic_workspaces(): boolean {
+        return this.mutter ? this.mutter.get_boolean("dynamic-workspaces") : false;
     }
 
     gap_inner(): number {
@@ -97,7 +101,7 @@ export class ExtensionSettings {
 
     is_dark(): boolean {
         if (this.int) {
-            let theme = this.int.get_string('gtk-theme').toLowerCase();
+            let theme = this.int.get_string("gtk-theme").toLowerCase();
             return DARK.some(dark => theme.includes(dark))
         }
 
@@ -106,7 +110,7 @@ export class ExtensionSettings {
 
     is_dark_shell(): boolean {
         if (this.shell) {
-            let theme = this.shell.get_string('name').toLowerCase()
+            let theme = this.shell.get_string("name").toLowerCase()
             return DARK.some(dark => theme.includes(dark) || theme.length === 0)
         }
         return this.is_dark();
@@ -134,7 +138,7 @@ export class ExtensionSettings {
 
     workspaces_only_on_primary(): boolean {
         return this.mutter
-            ? this.mutter.get_boolean('workspaces-only-on-primary')
+            ? this.mutter.get_boolean("workspaces-only-on-primary")
             : false;
     }
 
