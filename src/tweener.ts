e654bfa32372fb9127ccd050a073ad251fdbe826
Jose Maranan <jmmaranan@users.noreply.github.com>
2020-10-08 16:06:10 -0400

    fix: Flicker tween observe near realtime

--- src/tweener.ts
+++ src/tweener.ts
@@ -15,6 +15,7 @@ export function add(win: ShellWindow, p: TweenParams) {
     let a = win.meta.get_compositor_private();
     if (!p.mode) p.mode = Clutter.AnimationMode.LINEAR;
     if (a) {
+        remove(a);
         win.hide_border();
         win.update_border_layout();
         a.ease(p);
@@ -35,10 +36,21 @@ export function is_tweening(a: Clutter.Actor) {
 export function on_window_tweened(win: ShellWindow, callback: () => void): SignalID {
     win.update_border_layout();
     win.hide_border();
-    return GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+
+    // the amount of delay for recursively checking
+    // if the animation is done.
+    const tween_timeout = 20;
+    
+    return GLib.timeout_add(GLib.PRIORITY_DEFAULT, tween_timeout, () => {
         const actor = win.meta.get_compositor_private();
-        if (actor && is_tweening(actor)) return true;
-        callback();
+        if (actor) {
+            if (is_tweening(actor)) {
+                return true;
+            } else {
+                remove(actor);
+                callback();
+            }
+        }
         return false;
     });
 }
