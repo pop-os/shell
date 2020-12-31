244a007bec601b3e6d332b963b00718e2137bd46
Michael Aaron Murphy <mmstick@pm.me>
2020-12-30 17:13:34 -0700

    feat(tiling): Improved accuracy of keyboard movements across displays

--- src/geom.ts
+++ src/geom.ts
@@ -34,29 +34,37 @@ export function west(rect: Rectangular): [number, number] {
     return [rect.x, ycenter(rect)];
 }
 
-export function directional_distance(win_a: Meta.Window, win_b: Meta.Window, fn_a: (rect: Rectangular) => [number, number], fn_b: (rect: Rectangular) => [number, number]) {
-    let [ax, ay] = fn_a(win_a.get_frame_rect());
-    let [bx, by] = fn_b(win_b.get_frame_rect());
+export function distance([ax, ay]: [number, number], [bx, by]: [number, number]): number {
+    return Math.sqrt(Math.pow(bx - ax, 2) + Math.pow(by - ay, 2))
+}
 
-    return Math.sqrt(Math.pow(bx - ax, 2) + Math.pow(by - ay, 2));
+export function directional_distance(a: Rectangular, b: Rectangular, fn_a: (rect: Rectangular) => [number, number], fn_b: (rect: Rectangular) => [number, number]) {
+    return distance(fn_a(a), fn_b(b));
 }
 
 export function window_distance(win_a: Meta.Window, win_b: Meta.Window) {
-    return directional_distance(win_a, win_b, center, center);
+    return directional_distance(win_a.get_frame_rect(), win_b.get_frame_rect(), center, center);
 }
 
 export function upward_distance(win_a: Meta.Window, win_b: Meta.Window) {
-    return directional_distance(win_a, win_b, south, north);
+    return directional_distance(win_a.get_frame_rect(), win_b.get_frame_rect(), south, north);
 }
 
 export function rightward_distance(win_a: Meta.Window, win_b: Meta.Window) {
-    return directional_distance(win_a, win_b, west, east);
+    return directional_distance(win_a.get_frame_rect(), win_b.get_frame_rect(), west, east);
 }
 
 export function downward_distance(win_a: Meta.Window, win_b: Meta.Window) {
-    return directional_distance(win_a, win_b, north, south);
+    return directional_distance(win_a.get_frame_rect(), win_b.get_frame_rect(), north, south);
 }
 
 export function leftward_distance(win_a: Meta.Window, win_b: Meta.Window) {
-    return directional_distance(win_a, win_b, east, west);
+    return directional_distance(win_a.get_frame_rect(), win_b.get_frame_rect(), east, west);
+}
+
+export function shortest_side(origin: [number, number], rect: Rectangular): number {
+    let shortest = distance(origin, west(rect))
+    shortest = Math.min(shortest, distance(origin, north(rect)))
+    shortest = Math.min(shortest, distance(origin, east(rect)))
+    return Math.min(shortest, distance(origin, south(rect)))
 }
