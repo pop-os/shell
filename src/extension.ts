40acba0b17457acb37c8b8d7afa49cbe523095ae
Rongcui Dong <rongcuid@outlook.com>
2021-10-10 10:46:17 -0400

    Revert this to master

--- src/extension.ts
+++ src/extension.ts
@@ -274,7 +274,7 @@ export class Ext extends Ecs.System<ExtEvent> {
         switch (event.tag) {
             /** Callback Event */
             case 1:
-                (event.callback())
+                (event.callback)();
                 break
 
             /** Window Event */
