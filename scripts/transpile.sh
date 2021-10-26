af9caaee3c19e5f70be4ad153a8aefacb9ae63b9
Ava Barron <mztriz@gmail.com>
2021-10-26 10:41:58 -0400

    Update transpile.sh

--- scripts/transpile.sh
+++ scripts/transpile.sh
@@ -5,12 +5,12 @@ pwd=$(pwd)
 
 # In goes standard JS. Out comes GJS-compatible JS
 transpile() {
-    cat ${src} | sed -e 's#export function#function#g' \
+    cat "${src}" | sed -e 's#export function#function#g' \
         -e 's#export var#var#g' \
         -e 's#export const#var#g' \
         -e 's#Object.defineProperty(exports, "__esModule", { value: true });#var exports = {};#g' \
         | sed -E 's/export class (\w+)/var \1 = class \1/g' \
-        | sed -E "s/import \* as (\w+) from '(\w+)'/const \1 = Me.imports.\2/g" > ${dest}
+        | sed -E "s/import \* as (\w+) from '(\w+)'/const \1 = Me.imports.\2/g" > "${dest}"
 }
 
 rm -rf _build
@@ -20,8 +20,8 @@ glib-compile-schemas schemas &
 # Transpile to JavaScript
 
 for proj in ${PROJECTS}; do
-    mkdir -p _build/${proj}
-    tsc --p src/${proj}
+    mkdir -p _build/"${proj}"
+    tsc --p src/"${proj}"
 done
 
 tsc
@@ -33,7 +33,7 @@ wait
 cp -r metadata.json icons schemas *.css _build &
 
 for src in $(find target -name '*.js'); do
-    dest=$(echo $src | sed s#target#_build#g)
+    dest=$(echo "$src" | sed s#target#_build#g)
     transpile
 done
 
